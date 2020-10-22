35d80cb4f926f24b50bdc8c4e2701ca4f0d0fbe1
Casto Rodríguez <casrodia@gmail.com>
2020-10-22 10:41:31 +0200

    VARIABLES changes

--- out/production/procesador/gen/Analex.java
+++ out/production/procesador/gen/Analex.java
@@ -1,4 +1,4 @@
-// Generated from C:/Users/casto/IdeaProjects/procesador/src\Analex.g4 by ANTLR 4.8
+// Generated from C:/Users/casto/IdeaProjects/procesador/out/production/procesador\Analex.g4 by ANTLR 4.8
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
